c84e52b4b07f43fcffa3bc65b6254c5967293071
IT-POOL <168464152+IT-POOL@users.noreply.github.com>
2024-04-30 03:36:53 +0000

    update

--- day-1/basic.js
+++ day-1/basic.js
@@ -1,4 +1,29 @@
-// basics of javascrip
+/*
+Variables are nothing but a memory allocation created by JS to store the data.
+*/
 
-let username="akanksha"
-console.log(username)
+let a; // Declaration
+
+a = 15; // Definition
+
+console.log(a) // Call or use
+
+
+let userName = "Avinash"
+const age = 32
+var email = "avi@gmail.com"
+userCity = "Jabalpur"
+let isActive = True
+
+console.log(userName)
+
+console.table([userName, age, email, userCity])
+
+// let = Mutable Value
+// const = Immutable Value
+
+userName = "Atul"
+
+// Const values can not be changed once defined. You can only change Let values.
+
+console.table([userName, age])
