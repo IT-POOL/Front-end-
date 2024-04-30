b5fbd794e9477f32586b780e5787e70143054091
IT-POOL <168464152+IT-POOL@users.noreply.github.com>
2024-04-30 03:37:22 +0000

    datatypes

---
+++ day-1/datatypes.js
@@ -0,0 +1,21 @@
+// Primitive Data Types
+// Non Primitive Data Types
+
+/*
+Primitive Data Types -- 
+
+1. String -- Character Sets
+2. Number -- 0,1.2,1/4
+3. Boolean -- True, False
+4. Symbol -- Unique Data Type
+
+*/
+
+/*
+Non Prmitive Data Types -- 
+
+1. Array
+2. Object
+3. Function
+
+*/
